f01a481ae400dd5fd26091e1875296df0b8c765d
samaaMohamed <samaamohammed15@gmail.com>
2020-03-01 16:21:36 -0800

    Layout Header & footer already done

---
+++ src/shared/footer/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <h3>Abou Us</h3>
+              <p>
+                Construction SRL works as international construction services
+                company and is a leading builder in diverse and numerous market
+                segments. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit,
+              </p>
+            </div>
+            <div className="col-sm">
+              <h3>Contact Us</h3>
+              <p>
+                <span>
+                  <i class="far fa-envelope-open"></i>
+                </span>
+                Construction SRL works as international construction services
+                company and is a leading builder in diverse and numerous market
+                segments. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit,
+              </p>
+            </div>
+            <div className="col-sm">
+              <h3>Information</h3>
+              <p>
+                Construction SRL works as international construction services
+                company and is a leading builder in diverse and numerous market
+                segments. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit,
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
