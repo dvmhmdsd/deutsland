97c07da22a7c3f5c303fa18b82a169d1a7a55247
mohamedsaad <mahameds19999@gmail.com>
2020-03-07 20:01:29 +0200

    Edits v2

--- src/modules/main/submodules/home-page/components/intro/index.js
+++ src/modules/main/submodules/home-page/components/intro/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import "./style.css";
-import VideoBg from "../../../../../../shared/bg-video";
+import VideoBg from "shared/bg-video";
+
+import {Link} from "react-router-dom";
 
 export default class Intro extends Component {
   render() {
     return (
       <div className="intro">
         <VideoBg title="Water mist" buttonLabel="Read More"  />
-        <div className="detpedia">
+        <div className="dtpedia">
           <div className="container">
             <p>
               Get a free quote now or ask us a question, it's free, simple and
               fast. We will be happy to answer you
             </p>
-            <button>DTpedia</button>
+            <Link className="dtpedia-link" to="/dtpedia">DTpedia</Link>
           </div>
         </div>
       </div>
