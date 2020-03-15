64e69342da1368c8672d9b3916d877e8e3279f59
mohamedsaad <mahameds19999@gmail.com>
2020-03-15 10:51:08 +0200

    Make the layout of the projects page

---
+++ src/modules/projects/components/page/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Layout from "shared/layout";
+
+export default class ProjectsPage extends Component {
+  render() {
+    return (
+      <Layout>
+          <div className="section">
+              <p>projects page is ready</p>
+          </div>
+      </Layout>
+    )
+  }
+}
