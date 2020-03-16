656f10e92b6ed2235566757a3887a9bb6f07dd20
samaaMohamed <samaamohammed15@gmail.com>
2020-03-16 04:30:40 -0700

    Careers

--- src/modules/careers/components/item-page/index.js
+++ src/modules/careers/components/item-page/index.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import itemImage from "../../../../assets/partners/angus-circle.png";
+import "./style.css";
 
 export default class CareerItemPage extends Component {
   render() {
     return (
-      <>
-        <p> Career Item page is ready. </p>
-      </>
-    )
+      <div className="item-page">
+        <img src={itemImage} />
+        <div className="content">
+          <h4>{this.props.data.title}</h4>
+          <p>{this.props.data.body}</p>
+          <button>Apply Now</button>
+        </div>
+      </div>
+    );
   }
 }
